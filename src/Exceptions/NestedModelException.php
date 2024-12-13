ece5dca86f61f9f125169f27eb9d231bab17257f
michalsn <michal@sniatala.pl>
2024-12-13 08:14:29 +0100

    add missing strict-type declarations

--- src/Exceptions/NestedModelException.php
+++ src/Exceptions/NestedModelException.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Michalsn\CodeIgniterNestedModel\Exceptions;
 
 use RuntimeException;
--- tests/_support/Entities/Comment.php
+++ tests/_support/Entities/Comment.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Support\Entities;
 
 use CodeIgniter\Entity\Entity;
